695e0c2f379af85c4f8fd0d89924e4281789db34
Henrique Riccio <henrique.riccio@gmail.com>
2020-06-17 17:23:25 -0300

    minimum working version

---
+++ src/index.js
@@ -0,0 +1,87 @@
+import express from 'express'
+import {
+    v4 as uuidv4
+} from 'uuid'
+import morgan from 'morgan'
+
+const contacts = [{
+        'Id': uuidv4(),
+        'FirstName': "Albert",
+        'LastName': "Einstein",
+        'PhoneNumber': "2222-1111"
+    },
+    {
+        'Id': uuidv4(),
+        'FirstName': "Mary",
+        'LastName': "Curie",
+        'PhoneNumber': "1111-1111"
+    }
+]
+
+const app = express()
+
+app.use(express.json())
+app.use(morgan('tiny'))
+
+app.post('/', (req, res) => {
+    let contact = req.body
+
+    if (contact == null) {
+        res.sendStatus(400)
+        return
+    }
+    contact.Id = uuidv4()
+
+    contacts.push(contact)
+
+    //todo: 
+    //res.setHeader('Location',`${req.path}/${contact.Id}`)
+    res.setHeader('Location',`/${contact.Id}`)
+    res.sendStatus(201)
+})
+
+app.get('/', (req, res) => {
+    res.json(contacts)
+})
+
+app.get('/:id', (req, res) => {
+    let id = req.params.id
+    let contact = contacts.find(c => c.Id === id)
+    if (!contact) {
+        res.sendStatus(404)
+        return
+    }
+    res.json(contact)
+})
+
+app.put('/:id', (req, res) => {
+    let new_contact = req.body
+    if (new_contact == null) {
+        res.sendStatus(400)
+        return
+    }
+
+    let id = req.params.id
+    let i = contacts.findIndex(c => c.Id === id)
+    if (i === -1) {
+        res.sendStatus(404)
+        return
+    }
+
+    contacts[i] = new_contact
+
+    res.sendStatus(204)
+})
+
+app.delete('/:id', (req, res) => {
+    let id = req.params.id
+    let i = contacts.findIndex(c => c.Id === id)
+    if (i === -1) {
+        res.sendStatus(404)
+        return
+    }
+    contacts.splice(i, 1)
+    res.sendStatus(204)
+})
+
+app.listen(8010, () => console.log('server started'))
