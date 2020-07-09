6260cc2d8187d291ea6968f8fa9cc048ba717391
Henrique Riccio <henrique.riccio@gmail.com>
2020-07-09 18:11:49 -0300

    routes adjustment

--- src/index.js
+++ src/index.js
@@ -1,29 +1,26 @@
 import express from 'express'
-import {
-    v4 as uuidv4
-} from 'uuid'
+import { v4 as uuidv4 } from 'uuid'
 import morgan from 'morgan'
 
 const contacts = [{
-        'Id': uuidv4(),
-        'FirstName': "Albert",
-        'LastName': "Einstein",
-        'PhoneNumber': "2222-1111"
-    },
-    {
-        'Id': uuidv4(),
-        'FirstName': "Mary",
-        'LastName': "Curie",
-        'PhoneNumber': "1111-1111"
-    }
-]
+    'Id': uuidv4(),
+    'FirstName': "Albert",
+    'LastName': "Einstein",
+    'PhoneNumber': "2222-1111"
+},
+{
+    'Id': uuidv4(),
+    'FirstName': "Mary",
+    'LastName': "Curie",
+    'PhoneNumber': "1111-1111"
+}]
 
 const app = express()
 
 app.use(express.json())
 app.use(morgan('tiny'))
 
-app.post('/', (req, res) => {
+app.post('/contacts', (req, res) => {
     let contact = req.body
 
     if (contact == null) {
@@ -36,15 +33,15 @@ app.post('/', (req, res) => {
 
     //todo: 
     //res.setHeader('Location',`${req.path}/${contact.Id}`)
-    res.setHeader('Location',`/${contact.Id}`)
+    res.setHeader('Location', `/${contact.Id}`)
     res.sendStatus(201)
 })
 
-app.get('/', (req, res) => {
+app.get('/contacts', (req, res) => {
     res.json(contacts)
 })
 
-app.get('/:id', (req, res) => {
+app.get('/contacts/:id', (req, res) => {
     let id = req.params.id
     let contact = contacts.find(c => c.Id === id)
     if (!contact) {
@@ -54,7 +51,7 @@ app.get('/:id', (req, res) => {
     res.json(contact)
 })
 
-app.put('/:id', (req, res) => {
+app.put('/contacts/:id', (req, res) => {
     let new_contact = req.body
     if (new_contact == null) {
         res.sendStatus(400)
@@ -73,7 +70,7 @@ app.put('/:id', (req, res) => {
     res.sendStatus(204)
 })
 
-app.delete('/:id', (req, res) => {
+app.delete('/contacts/:id', (req, res) => {
     let id = req.params.id
     let i = contacts.findIndex(c => c.Id === id)
     if (i === -1) {
